463c39b368601848a875942b4bbaedd35d19cf52
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2025-01-22 22:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function removeColor() {
+            const selectElement = document.getElementById('colorSelect'); // Get the select element
+            const selectedOption = selectElement.options[selectElement.selectedIndex]; // Get the selected option
+
+            // Remove the selected option from the dropdown list
+            selectElement.removeChild(selectedOption);
+        }
